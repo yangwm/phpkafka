ab2766a77e94bc16191ff44005385be81de90959
Elias Van Ootegem <elias.vanootegem@wimionline.be>
2015-04-17 11:22:05 +0200

    Add CI to main dev branch, too

---
+++ travis.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+echo "....fetching librdkafka dependency...."
+mkdir tmp_build
+cd tmp_build
+## clone fork, we know this version of librdkafka works
+git clone https://github.com/EVODelavega/librdkafka.git
+echo ".....done....."
+cd librdkafka
+echo "....compiling librdkafka...."
+./configure && make
+sudo make install
+echo "....done...."
+cd ../../
+echo ".... ensure librdkafka is available....."
+sudo ldconfig
+rm -Rf tmp_build
+echo ".... start building extension....."
+phpize
+./configure --enable-kafka
+make
+NO_INTERACTION=1 make test
