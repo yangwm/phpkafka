ae0d4659b3409695687ddeb7a78e4c8f4b50b994
yangwumming <yangwuming@panda.tv>
2016-01-12 14:37:58 +0800

    phpkakfa: test php

--- lib/setup.sh
+++ lib/setup.sh
@@ -1,8 +1,8 @@
 
 cp ~/phpkafka/
 sudo cp lib/librdkafka* /usr/local/lib/
-sudo cp lib/centos-6.7-64.bit/php-5.3/kafka.so /usr/local/php-5.3/lib/php/extensions/
-sudo cp lib/centos-6.7-64.bit/php-5.6/kafka.so /usr/local/php-5.6/lib/php/extensions/
+sudo cp lib/centos-6.7-64.bit/php-5.3/kafka.so /usr/local/php-5.3/extensions/
+sudo cp lib/centos-6.7-64.bit/php-5.6/kafka.so /usr/local/php-5.6/extensions/
 
 sudo ldconfig
 
