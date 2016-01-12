7458c1338ef5c1288a07608052ea00e20e0e27ce
yangwm <jxfzywm@163.com>
2016-01-12 16:59:05 +0800

    phpkafka: lib/setup.sh

--- lib/setup.sh
+++ lib/setup.sh
@@ -1,14 +1,15 @@
 
-cp ~/phpkafka/
+cd ~/phpkafka/
 sudo cp lib/librdkafka* /usr/local/lib/
 sudo cp lib/centos-6.7-64.bit/php-5.3/kafka.so /usr/local/php-5.3/extensions/
 sudo cp lib/centos-6.7-64.bit/php-5.6/kafka.so /usr/local/php-5.6/extensions/
 
-sudo ldconfig
-
-cp /usr/local/lib/
+cd /usr/local/lib/
 sudo ln -s librdkafka.so.1 librdkafka.so
 sudo ln -s librdkafka++.so.1 librdkafka++.so
 
-php -c kafka.ini testkafka.php
+sudo ldconfig
+
+cd ~/phpkafka/
+php -c lib/kafka.ini lib/testkafka.php
 
