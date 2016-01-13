5ae1b335955ba0895c3e90acad54810950873011
yangwm <jxfzywm@163.com>
2016-01-13 16:30:17 +0800

    phpkakfa: add librdkafka lib setup.sh

--- lib/setup.sh
+++ lib/setup.sh
@@ -1,12 +1,16 @@
 
-cd ~/phpkafka/
-sudo cp lib/librdkafka* /usr/local/lib/
-sudo cp lib/centos-6.7-64.bit/php-5.3/kafka.so /usr/local/php-5.3/extensions/
-sudo cp lib/centos-6.7-64.bit/php-5.6/kafka.so /usr/local/php-5.6/extensions/
+cd ~/phpkafka/lib/
+sudo cp librdkafka* /usr/local/lib/
+sudo cp centos-6.7-64.bit/php-5.3/kafka.so /usr/local/php-5.3/extensions/
+sudo cp centos-6.7-64.bit/php-5.6/kafka.so /usr/local/php-5.6/extensions/
 
 cd /usr/local/lib/
 sudo ln -s librdkafka.so.1 librdkafka.so
 sudo ln -s librdkafka++.so.1 librdkafka++.so
+
+ls -ls librdkafka*
+ls -ls /usr/local/php-5.3/extensions/kafka.so
+ls -ls /usr/local/php-5.6/extensions/kafka.so
 
 sudo ldconfig
 
